e30e9d71adbcbaa579a7b38cd3fe461b9cb0cdf5
LordPhoenix19 <sabaldanajunerey@gmail.com>
2025-02-25 13:07:11 +0800

    Updates

--- assets/keyboard2-legacy-B7iQmWYe.js
+++ assets/keyboard2-legacy-B7iQmWYe.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dz9tRTwv.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DrbyQZYl.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
